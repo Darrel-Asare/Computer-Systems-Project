a4e20507e40f10fbe50b158f107040a5349e0f44
Darrelasare <55444998+Darrelasare@users.noreply.github.com>
2020-03-11 02:55:37 -0400

    Added python source for rover using threads

---
+++ app.py
@@ -0,0 +1,209 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import time
+import signal
+import sys
+import l293d.driver as l293d
+import logging
+import threading
+import rover_constants as rc
+
+stopThreads = False
+useKeys = False
+key = None
+
+motor1 = l293d.motor(rc.Motor1A, rc.Motor1B, rc.Motor1E)
+motor2 = l293d.motor(rc.Motor2A, rc.Motor2B, rc.Motor2E)
+
+# use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+
+
+# set GPIO input and output channels
+GPIO.setup(rc.pinTrigger, GPIO.OUT)
+GPIO.setup(rc.pinEcho, GPIO.IN)
+GPIO.setup(rc.sensor, GPIO.IN)
+
+# setup inputs
+GPIO.setup(rc.Motor1A, GPIO.OUT)
+GPIO.setup(rc.Motor1B, GPIO.OUT)
+GPIO.setup(rc.Motor1E, GPIO.OUT)
+
+GPIO.setup(rc.Motor2A, GPIO.OUT)
+GPIO.setup(rc.Motor2B, GPIO.OUT)
+GPIO.setup(rc.Motor2E, GPIO.OUT)
+
+
+def close():
+    global stopThreads
+
+    logging.info("\nMain    : Turning off sensors and motors...\n")
+
+    stopThreads = True
+    GPIO.cleanup()
+    l293d.cleanup()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, close)
+
+
+def set_start():
+    global start
+    start = time.time()
+
+
+def set_end():
+    global end
+    end = time.time()
+
+
+def get_rpm():
+    global count
+
+    if not count:
+        set_start()
+        count = count + 1
+    else:
+        count = count + 1
+
+    if count == rc.sample:
+        set_end()
+        delta = end - start
+        delta = delta / 60
+        rpm = (rc.sample / delta) / 2
+        print(rpm)
+        count = 0
+    else:
+        logging.info("rpm     : count is %d", count)
+
+
+GPIO.add_event_detect(rc.sensor, GPIO.RISING, callback=get_rpm)
+
+
+def thread_ds():
+    logging.info("Distance: Started RPM sensor thread")
+    while not stopThreads:
+        # set Trigger to HIGH
+        GPIO.output(rc.pinTrigger, True)
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(rc.pinTrigger, False)
+
+        # track the times
+        start_time = time.time()
+        stop_time = time.time()
+
+        # save start time
+        while (0 == GPIO.input(rc.pinEcho)) and not stopThreads:
+            start_time = time.time()
+
+        # save time of arrival
+        while (1 == GPIO.input(rc.pinEcho)) and not stopThreads:
+            stop_time = time.time()
+
+        # time difference between start and arrival
+        elapsed_time = stop_time - start_time
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (elapsed_time * 34300) / 2
+
+        logging.info("Distance: %.1f cm", distance)
+        time.sleep(0.1)
+
+
+def thread_rpm():
+    logging.info("rpm     : Started RPM sensor thread")
+    while not stopThreads:
+        time.sleep(0.1)
+
+
+def initialize_motors():
+    logging.info("Motors  : Initializing motors sequence")
+    # Run the motors so visible
+    for i in range(0, 150):
+        motor1.clockwise()
+    # Run the motors so visible
+    for i in range(0, 150):
+        motor2.clockwise()
+    # Run the motors so visible
+    for i in range(0, 150):
+        motor1.anticlockwise()
+        motor2.anticlockwise()
+
+
+def thread_motors():
+    logging.info("Motors  : Started motors thread")
+    initialize_motors()
+    while not stopThreads:
+        if key == "w":
+            # move forward
+            logging.info("Motors  : Moving forward")
+        if key == "a":
+            # move left
+            logging.info("Motors  : Turn left")
+        if key == "s":
+            # move backward
+            logging.info("Motors  : Moving backward")
+        if key == "d":
+            # move right
+            logging.info("Motors  : Turn right")
+
+
+def thread_keys():
+    global key
+    while useKeys and not stopThreads:
+        key = input().lower()
+
+
+def thread_start(name):
+    logging.info("Thread '%s': starting", name)
+    if name == "rpm":
+        thread_rpm()
+    elif name == "ds":
+        thread_ds()
+    elif name == "motors":
+        thread_motors()
+
+    logging.info("Thread '%s': finishing", name)
+
+
+if __name__ == "__main__":
+    log_format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=log_format, level=logging.INFO, datefmt="%H:%M:%S")
+
+    logging.info("Main    : Welcome to the RPM, Distance and Motor Rover")
+    logging.info('Main    : Number of arguments:', len(sys.argv), 'arguments.')
+    logging.info('Main    : Argument List:', str(sys.argv))
+    useKeys = (sys.argv[1] == "useKeys")
+
+    if useKeys:
+        logging.info("Main    : Use 'W A S D' (move) and '<Space Bar>' (stop) to control the rover!")
+
+    threads = list()
+    rpmThread = threading.Thread(target=thread_rpm, args=("rpm",))
+    threads.append(rpmThread)
+    dsThread = threading.Thread(target=thread_ds, args=("ds",))
+    threads.append(dsThread)
+    motorThreads = threading.Thread(target=thread_motors, args=("motors",))
+    threads.append(motorThreads)
+    if useKeys:
+        threads.append(threading.Thread(target=thread_keys, args=("keys",)))
+
+    try:
+        # start the threads
+        for index in range(4):
+            threads[index].start()
+
+        # join the threads
+        for index, thread in enumerate(threads):
+            logging.info("Main    : before joining thread %d.", index)
+            thread.join()
+            logging.info("Main    : thread %d done", index)
+    except KeyboardInterrupt:
+        stopThreads = True
+        print(">>> Quit")
+
+    # Done
+    logging.info("Main    : all done")
---
+++ rover_constants.py
@@ -0,0 +1,19 @@
+# set GPIO Pins
+pinTrigger = 7
+pinEcho = 11
+sensor = 21  # TODO: Change from BCM
+
+# motors
+Motor1A = 16
+Motor1B = 18
+Motor1E = 22
+
+Motor2A = 19
+Motor2B = 21
+Motor2E = 23
+
+# Counter variables
+sample = 10 # how many half revolutions to time
+count = 0
+start = 0
+end = 0
